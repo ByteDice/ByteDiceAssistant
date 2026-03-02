de2cf4dc64d352b45958679a7d2974eadbf86f41
ByteDice <bytedice.official@gmail.com>
2026-03-02 21:26:38 +0100

    fixed variable names

--- src/cmds/debug/whoami.rs
+++ src/cmds/debug/whoami.rs
@@ -4,8 +4,8 @@ pub async fn cmd(ctx: Context<'_>) -> Result<(), Error> {
   let data = ctx.data();
   let uid: u64 = ctx.author().id.into();
 
-  let is_owner  = data.owners .contains(&uid);
-  let is_bk_mod = data.bk_mods.contains(&uid);
+  let is_owner  = data.env_vars.bot_owners.contains(&uid);
+  let is_bk_mod = data.env_vars.reddit_mod_discord_ids.contains(&uid);
 
   send_msg(ctx, lang!("dc_msg_whoami", is_owner, is_bk_mod), true, true).await;
 
